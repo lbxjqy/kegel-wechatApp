c05babe3f4de946bfe10146e303fcb26e2b50bb7
linboxuan <377760330@qq.com>
2020-07-30 20:44:06 +0800

    two commit

--- koa2-server/controller/user.js
+++ koa2-server/controller/user.js
@@ -2,6 +2,7 @@ const User = require('../schema/user')
 const Stage = require('../schema/stage')
 const result = require('../public/utils/result')
 const moment = require('moment')
+const sequelize = require('sequelize')
 class userController {
     /**
      * 创建用户
@@ -38,19 +39,22 @@ class userController {
      */
     static async endTrain(ctx,next) {
         let body = ctx.request.body;
+        // 查询处训练时间
         let stage = await Stage.findById(body.stageId);
         if(!stage) {
             ctx.body = result.getResultFiled(null, "stageId is error")
-            // return 不执行后面操作
             return;
         }
-        let u = await User.findOne({
-            attributes: ["id","createdAt","updatedAt","nickname","integral","stage","trainTime"],
-            where: {
-                id:ctx.query.uid,
-            }
-        })
-        u.trainTime += body.time
+        // 修改用户表累计训练时间
+        // UPDATE `user` SET `train_time`=train_time+90 WHERE ( `id` = '1')
+        let res = await User.update({trainTime:sequelize.literal('train_time+' + stage.trainTime)},{where:{id:body.uid}})
+        if(res.length[0]) {
+            ctx.body = result.getResultFiled(null, "train_time is error")
+            return;
+        }
+        let u = await User.findById(body.uid,{
+            attributes: ["id","trainTime"]})
+        ctx.body = result.getResultSuccess(u);
         next();
     }
 }
